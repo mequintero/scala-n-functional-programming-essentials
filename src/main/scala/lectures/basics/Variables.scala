ac086d93c488c5045b17b5b294352ea496615518
mequintero <mequintero@personalsoft.com.co>
2024-05-13 15:51:30 -0500

    gitignore all files from .idea

--- src/main/scala/lectures/basics/Variables.scala
+++ src/main/scala/lectures/basics/Variables.scala
@@ -3,7 +3,7 @@ package lectures.basics
 object Variables extends App {
   private var any = 1
   any = Values.two
-  println("%d length(%d)".format(any, any.toString.length))
+  println("%d length(%s)".format(any, any.toString.length))
   // variables can mutate only of value. type variables is immutable
   any = Values.x.length
   print(Values.x + s" length($any)")
