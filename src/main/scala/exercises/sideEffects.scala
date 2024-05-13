8c69e40994502b27347937afc3492b20229281fd
mequintero <mequintero@personalsoft.com.co>
2024-05-13 13:16:47 -0500

    Exercises package

---
+++ src/main/scala/exercises/sideEffects.scala
@@ -0,0 +1,13 @@
+package exercises
+
+object sideEffects {
+  def main(args: Array[String]): Unit = {
+    val greeting = {
+      val name = if (args.length > 0) args(0) else "World"
+      "hello" + " " + name + "!"
+      "hello%s !".format(if name == "World" then " " + name else name)
+      s"Hello $name!"
+    }
+    println(greeting)
+  }
+}
