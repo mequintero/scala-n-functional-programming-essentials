7c58f7542393a1497edef49bd37a34fb76fc2c50
mequintero <mequintero@personalsoft.com.co>
2024-05-13 15:39:13 -0500

    deprecation fix
    println "%d length(%d)" ...

--- src/main/scala/lectures/basics/Variables.scala
+++ src/main/scala/lectures/basics/Variables.scala
@@ -3,7 +3,7 @@ package lectures.basics
 object Variables extends App {
   private var any = 1
   any = Values.two
-  println(any + " length(%s)".format(any.toString.length))
+  println("%d length(%d)".format(any, any.toString.length))
   // variables can mutate only of value. type variables is immutable
   any = Values.x.length
   print(Values.x + s" length($any)")
